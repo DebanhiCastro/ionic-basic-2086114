7897d53950b20b258a07a06fa78546b984a0fc1f
Astrid <158233818+DebanhiCastro@users.noreply.github.com>
2024-05-12 18:55:57 -0600

    Practica9

--- ionic-basic/app-routing.module.ts
+++ ionic-basic/app-routing.module.ts
@@ -47,3 +47,62 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+{
+  path: '',
+  redirectTo: 'home',
+  pathMatch: 'full'
+},
+{
+  path: 'presupuesto',
+  loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule),
+  canActivate:[GuardianGuard]
+},
+{
+  path: 'alumnos',
+  loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
+},
+{
+  path: 'inicio',
+  loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+},
+{
+  path: 'receptor',
+  loadChildren: () => import('./receptor/receptor.module').then( m => m.ReceptorPageModule)
+},
+{
+  path: 'receta',
+  loadChildren: () => import('./receta/receta.module').then( m => m.RecetaPageModule)
+},
+{
+  path: 'detalle-receta',
+  loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule)
+},
+{
+  path: 'tabs',
+  loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+},  {
+  path: 'login',
+  loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+},
+{
+  path: 'logout',
+  loadChildren: () => import('./logout/logout.module').then( m => m.LogoutPageModule)
+},
+{
+  path: 'register',
+  loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+},
+{
+  path: 'destinos',
+  loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
+},
+
+
+];
+@NgModule({
+imports: [
+  RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+],
+exports: [RouterModule]
+})
+export class AppRoutingModule { }
